7e76478cbc040db81268cbfaf8504795bd909143
zzho-o <78499273+zzho-o@users.noreply.github.com>
2024-12-11 00:40:26 +0900

    feat:markdown

--- React/react-test/src/components/Markdown/index.js
+++ React/react-test/src/components/Markdown/index.js
@@ -5,27 +5,29 @@ const Markdown = ({ markdown }) => {
     const renderer = new marked.Renderer();
 
     // Heading에 앵커추가
-    renderer.heading = (text, num) => {
-        const id = text.toLowerCase().replace(/\s+/g, "-") //id의 공백제거
+    renderer.heading = (heading, num) => {
+        const headingText = heading.text.replace(/\n/g, '<br/>')
+        const id = heading.text.toLowerCase().replace(/\s+/g, "-") //id의 공백제거
         return `
             <h${num} id="${id}">
-                <a href="#${id}">${text}</a>
+                <a href="#${id}">${headingText}</a>
             </h${num}>
-        `;
-    };
+        `
+    }
 
     // Blockquote 클릭 시 복사 기능 추가
     renderer.blockquote = (quote) => {
+        const quoteText = quote.text.replace(/\n/g, '<br/>'); // 줄바꿈을 <br/>로 변환
         return `
-            <blockquote style="cursor: pointer;" onclick="navigator.clipboard.writeText('${quote}')">
-                ${quote}
+            <blockquote style="cursor: pointer;" onclick="navigator.clipboard.writeText('${quoteText}')">
+                ${quoteText}
             </blockquote>
         `
-    };
+    }
 
     // marked를 사용해 마크다운을 HTML로 변환
     const html = marked(markdown, { renderer })
-
+    console.log(html)
     return (
         <div
             dangerouslySetInnerHTML={{ __html: html }}
